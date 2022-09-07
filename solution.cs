f2b81660a47ab19584c714717fec88de6318c44e
Kooreis <kooreis.official@gmail.com>
2022-09-07 14:02:35 -0500

    Added a condition to check if the last characters of the strings are the same. If they are, the function calls itself with the lengths of the strings reduced by 1.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,7 @@
 function editDistance(str1, str2, m, n) {
     if (m == 0) return n;
     if (n == 0) return m;
+
+    if (str1[m - 1] == str2[n - 1])
+        return editDistance(str1, str2, m - 1, n - 1);
 }
